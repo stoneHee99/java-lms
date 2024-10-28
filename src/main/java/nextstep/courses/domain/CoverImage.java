969037b2ee7e7f97d8daf6dc52a1addf0be77e49
Seokhee Park <seokhee_park@nexcubecorp.com>
2024-10-28 16:01:20 +0900

    feat: 이미지 파일 크기가 1MB 이상일때 예외 발생

--- src/main/java/nextstep/courses/domain/CoverImage.java
+++ src/main/java/nextstep/courses/domain/CoverImage.java
@@ -4,10 +4,18 @@ import java.io.File;
 
 public class CoverImage {
 
+    private static final int MAX_FILE_SIZE = 1024 * 1024; // 1MB
+
     private final File imageFile;
 
-
     public CoverImage(File imageFile) {
+        valid(imageFile);
         this.imageFile = imageFile;
+    }
+
+    private void valid(File imageFile) {
+        if (imageFile.length() > MAX_FILE_SIZE) {
+            throw new IllegalArgumentException("이미지 파일 크기는 1MB를 넘을 수 없습니다.");
+        }
     }
 }
