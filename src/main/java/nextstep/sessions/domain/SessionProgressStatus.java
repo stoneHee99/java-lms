944a2352f95ed6f44e617b0cbddcf8cf2bf1da84
Seokhee Park <seokhee_park@nexcubecorp.com>
2024-11-07 10:27:06 +0900

    refactor: SessionProgressStatus description 삭제

--- src/main/java/nextstep/sessions/domain/SessionProgressStatus.java
+++ src/main/java/nextstep/sessions/domain/SessionProgressStatus.java
@@ -1,18 +1,7 @@
 package nextstep.sessions.domain;
 
 public enum SessionProgressStatus {
-    PREPARING("준비중"),
-    IN_PROGRESS("진행중"),
-    FINISHED("종료");
-
-    private final String description;
-
-    SessionProgressStatus(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public String toString() {
-        return description;
-    }
+    PREPARING,
+    IN_PROGRESS,
+    FINISHED;
 }
